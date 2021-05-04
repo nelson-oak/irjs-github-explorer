9f2cf69626bde0bee7f847c2940173dbbfa5353a
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-04 19:55:05 -0300

    Aula 'Utilizando o useEffect' (C1-M1-SM3)

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
+import { useEffect, useState } from "react";
 
 const repository = {
   name: 'nc-shiny-counter-backend',
@@ -9,6 +10,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/nelson-oak/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
